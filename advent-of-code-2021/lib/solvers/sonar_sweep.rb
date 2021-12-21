6bb406f2fc47233b64fe6a3598f8118624210c3c
Yaquelqun <sandjiv@drawbotics.com>
2021-12-21 11:03:34 +0400

    part 2 make it work

--- advent-of-code-2021/lib/solvers/sonar_sweep.rb
+++ advent-of-code-2021/lib/solvers/sonar_sweep.rb
@@ -1,24 +1,35 @@
+# frozen_string_literal: true
+
 require_relative "../helpers/input_parser"
 
 module AdventOfCode2021
   module Solvers
+    # Computes height changes given a list of depths
     class SonarSweep
-
       def initialize
-        @data = Helpers::InputParser.new(endpoint: 'day1_input').parse_data.map(&:to_i)
+        @data = Helpers::InputParser.new(endpoint: "day1_input").parse_data.map(&:to_i)
       end
 
       def solve
         puts "number of increases: #{part1_solution}"
+        puts "number of summed increases: #{part2_solution}"
       end
 
       private
+
       attr_reader :data
 
       def part1_solution
         increase_number = 0
         data[1..].each_with_index { |number, index| increase_number += 1 if data[index] < number }
-        return increase_number
+        increase_number
+      end
+
+      def part2_solution
+        summed_data = data[2..].map.with_index { |number, index| number + data[index] + data[index + 1] }
+        increase_number = 0
+        summed_data[1..].each_with_index { |number, index| increase_number += 1 if summed_data[index] < number }
+        increase_number
       end
     end
   end
