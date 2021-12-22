e22ac9130a4ff827380343546d85a2f4439fd4f9
Yaquelqun <sandjiv@drawbotics.com>
2021-12-22 15:14:04 +0400

    part1: make it work

--- advent-of-code-2021/lib/solvers/vents_mapper.rb
+++ advent-of-code-2021/lib/solvers/vents_mapper.rb
@@ -7,7 +7,7 @@ module AdventOfCode2021
     # simulate bingo rounds
     class VentsMapper
       def initialize
-        @data = Helpers::InputParser.new(endpoint: "day4_input").parse_data
+        @data = Helpers::InputParser.new(endpoint: 'day5_input').parse_data
       end
 
       def solve
@@ -19,9 +19,38 @@ module AdventOfCode2021
 
       attr_reader :data
 
-      def part1_solution; end
+      def part1_solution
+        map = [] # Could be a hash, will depend on part2
+        coordinates = parse_input
+        coordinates.each do |coordinate|
+          next unless coordinate[0][0] == coordinate[1][0] || coordinate[0][1] == coordinate[1][1]
+
+          row_range = [coordinate[0][0], coordinate[1][0]].sort
+          column_range = [coordinate[0][1], coordinate[1][1]].sort
+          (row_range.first..row_range.last).each do |row|
+            map[row] ||= []
+            (column_range.first..column_range.last).each do |column|
+              if map[row][column].nil?
+                map[row][column] = 1
+              else
+                map[row][column] += 1
+              end
+            end
+          end
+        end
+        map.flatten.compact.count { _1 > 1 }
+      end
 
       def part2_solution; end
+
+      def parse_input
+        data.map do |data_line|
+          data_line                 # "562,433 -> 241,754"
+          .split('->')              # ["562,433", "241,754"]
+          .map { _1.split(',') }     # [["562", "433"], ["241", "754"]]
+          .map { _1.map(&:to_i) }    # [[562, 433], [241, 754]]
+        end
+      end
     end
   end
 end
