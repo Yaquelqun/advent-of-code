23936a4f37c64f03971d20c563d3c8c8351653a9
Yaquelqun <sandjiv@drawbotics.com>
2021-12-22 17:50:46 +0400

    part 2 + refactoer

--- advent-of-code-2021/lib/solvers/lanternfish_watcher.rb
+++ advent-of-code-2021/lib/solvers/lanternfish_watcher.rb
@@ -8,37 +8,43 @@ module AdventOfCode2021
     class LanternFishWatcher
       def initialize
         @data = Helpers::InputParser.new(endpoint: "day6_input").parse_data
+
+        # @populations is a 9 element array.
+        # For each position, the value is the amount of fish that are `position day away from reproduction`
+        @populations = [0] + parse_input.tally.sort_by(&:first).map(&:last) + [0, 0, 0]
       end
 
       def solve
-        puts "lanternfish amount: #{part1_solution}" # Solution:
-        # puts "all vents intersection numbers: #{part2_solution}" # Solution: 22083
+        simulate_populations(days: 80)
+        puts "lanternfish amount after 80 days: #{populations.sum}" # Solution: 362740
+        simulate_populations(days: 176)
+        puts "lanternfish amount after 256 days: #{populations.sum}" # Solution: 1644874076764
       end
 
       private
 
-      attr_reader :data
+      attr_reader :data, :populations
 
-      def part1_solution
-        populations = [0] + parse_input.tally.sort_by(&:first).map(&:last) + [0, 0, 0]
-        80.times do |index|
+      def simulate_populations(days:)
+        days.times do
           next_generation = populations.first
-          populations.map.with_index do |population, countdown|
-            next if countdown.zero?
-
-            populations[countdown - 1] = population
-            populations[countdown] = 0
-          end
+          simulate_day
           populations[6] += next_generation
           populations[8] += next_generation
         end
-        populations.sum
       end
 
-      def part2_solution; end
+      def simulate_day
+        populations.map.with_index do |population, countdown|
+          next if countdown.zero?
+
+          populations[countdown - 1] = population
+          populations[countdown] = 0
+        end
+      end
 
       def parse_input
-        data.first.split(',').map(&:to_i)
+        data.first.split(",").map(&:to_i)
       end
     end
   end
