560242a99efc6fdd9623e47ae3eb90bbddfd1fd5
Yaquelqun <sandjiv@drawbotics.com>
2021-12-22 10:26:19 +0400

    create simple bingo card logic

---
+++ advent-of-code-2021/lib/models/bingo_card.rb
@@ -0,0 +1,36 @@
+module AdventOfCode2021
+  module Models
+    # Represent a bingo card with the logic to tick a number and compute score
+    class BingoCard
+      attr_accesssor :card
+
+      def initialize(numbers)
+        @card = Matrix.rows(numbers)
+        @last_drawn_number = -1
+      end
+
+      def check_number(number)
+        @last_drawn_number = number
+        card.each_with_index do |value, row, column|
+          card[row, column] = -1 if value == number
+        end
+      end
+
+      def score
+        raise 'wtf' if @last_drawn_number == -1
+
+        @card.rows
+        .flatten
+        .reduce(0) { |acc, number| number == -1 ? acc : acc + number } * @last_drawn_number
+      end
+
+      def winning?
+        winning_score = (@card.row_count * -1)
+        return true if @cards.row_vectors.to_a.any? { _1.sum == winning_score }
+        return true if @cards.column_vectors.to_a.any? { _1.sum == winning_score }
+
+        false
+      end
+    end
+  end
+end
