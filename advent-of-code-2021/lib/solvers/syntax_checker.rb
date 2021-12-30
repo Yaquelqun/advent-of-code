8b6a0e1cdf556553fc3fd4a155cd40bec5c4820f
Yaquelqun <sandjiv@drawbotics.com>
2021-12-30 20:35:42 +0400

    part1 make it work

--- advent-of-code-2021/lib/solvers/syntax_checker.rb
+++ advent-of-code-2021/lib/solvers/syntax_checker.rb
@@ -1,5 +1,5 @@
 module AdventOfCode2021
-  module Sovers
+  module Solvers
     # Checks syntax of a line of code
     class SyntaxChecker
       def initialize
@@ -8,7 +8,7 @@ module AdventOfCode2021
 
 
       def solve
-        puts "total syntax error score: #{part1_solution}" # Solution: 
+        puts "total syntax error score: #{part1_solution}" # Solution: 243939
         puts ": #{part2_solution}" # Solution: 
       end
 
@@ -16,16 +16,43 @@ module AdventOfCode2021
 
       attr_reader :data
 
+      BRACKETS_DICTIONNARY = {
+        '(' => ')',
+        '{' => '}',
+        '[' => ']',
+        '<' => '>'
+      }.freeze
+
+      ILLEGAL_CHARACTER_SCORES = {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25_137
+      }.freeze
+
       def part1_solution
+        parsed_input.reduce(0) do |score, char_line|
+          score + check_line(char_line)
+        end
       end
 
       def part2_solution
       end
 
-      def parsed_input
-        @parsed_input ||= begin
-
+      def check_line(line)
+        expected_closers = []
+        line.each do |char|
+          if BRACKETS_DICTIONNARY.keys.include?(char)
+            expected_closers << BRACKETS_DICTIONNARY[char]
+          elsif char != expected_closers.pop
+            return ILLEGAL_CHARACTER_SCORES[char]
+          end
         end
+        0
+      end
+
+      def parsed_input
+        @parsed_input ||= data.map { _1.split('') }
       end
     end
   end
