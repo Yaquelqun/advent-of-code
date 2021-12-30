30452fef00282960682226538acf0afa92734f1c
Yaquelqun <sandjiv@drawbotics.com>
2021-12-30 23:19:52 +0400

    modelisation work and pseudo code part 1

--- advent-of-code-2021/lib/solvers/paths_finder.rb
+++ advent-of-code-2021/lib/solvers/paths_finder.rb
@@ -20,6 +20,13 @@ module AdventOfCode2021
       attr_reader :data
 
       def part1_solution
+        state = { incomplete_paths: [['start']], complete_paths: [] }
+        until incomplete_paths.empty?
+          # get the first path
+          # generate all possible paths from there
+          # next if no other path
+          # add complete paths to the complete paths and incomplete paths to the incomplete paths
+        end
       end
 
       def part2_solution
@@ -27,6 +34,26 @@ module AdventOfCode2021
 
       def parsed_input
         @parsed_input ||= data.map { _1.split("-") }
+      end
+
+      def nodes
+        @nodes ||= begin
+          nodes = Hash.new { |hash, key| hash[key] = { type: nil, neighbors: [] } }
+          parsed_input.each do |start, finish|
+            modify_node(nodes, start, finish)
+            modify_node(nodes, finish, start)
+          end
+          nodes
+        end
+      end
+
+      def modify_node(node_list, current_node, new_neighbor)
+        node_list[current_node][:neighbors] << new_neighbor
+        node_list[current_node][:type] ||= compute_type(current_node)
+      end
+
+      def compute_type(str)
+        str.downcase == str ? 'small' : 'big'
       end
     end
   end
