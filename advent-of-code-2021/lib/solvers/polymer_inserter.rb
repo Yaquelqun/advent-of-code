51683de9ea540ad5fbd75d8c3d997ba84dcb2d0a
Yaquelqun <sandjiv@drawbotics.com>
2022-01-10 18:56:38 +0400

    tried building the string, just realized that the end-string is probably a few terabytes long, so let's try something else

--- advent-of-code-2021/lib/solvers/polymer_inserter.rb
+++ advent-of-code-2021/lib/solvers/polymer_inserter.rb
@@ -11,7 +11,6 @@ module AdventOfCode2021
       end
 
       def solve
-        10.times { expand_rules }
         puts "most - least common element after 10 steps: #{part1_solution}" # Solution: 2435
         @submarine_polymer = nil
         puts ":#{part2_solution}" # Solution:
@@ -25,6 +24,11 @@ module AdventOfCode2021
         10.times do |step|
           puts '###########'
           puts step
+          puts "new_added_patterns: #{@added_patterns}"
+          puts "rule_book size: #{rules.keys.size}"
+          puts "largest_key: #{rules.keys.map(&:size).max}"
+          puts "polymer_size size: #{submarine_polymer.size}"
+          @added_patterns = 0
           expand_polymer
         end
         
@@ -36,6 +40,12 @@ module AdventOfCode2021
         40.times do |step|
           puts '###########'
           puts step
+          puts "new_added_patterns: #{@added_patterns}"
+          puts "rule_book size: #{rules.keys.size}"
+          puts "largest_key: #{rules.keys.map(&:size).max}"
+          puts "polymer_size size: #{submarine_polymer.size}"
+
+          @added_patterns = 0
           expand_polymer
         end
 
@@ -43,31 +53,40 @@ module AdventOfCode2021
         element_occurrences.last - element_occurrences.first
       end
 
-      def expand_polymer(polymer = submarine_polymer)
+      def expand_polymer(polymer = submarine_polymer, expanding_rules = false)
         pointer = 0
         until polymer[pointer + 1].nil?
+          print "pointer at #{pointer}/#{polymer.length}" + "\r" unless expanding_rules
           end_pointer = find_largest_known_pattern(polymer, pointer)
-          pointer += 1 && next if end_pointer == pointer
-
+          if end_pointer == pointer
+            pointer += 1
+            next
+          end
+          
           couple = polymer[pointer..end_pointer].join
           new_element = rules[couple]
+          # puts "found pattern of size #{couple.length}" if !expanding_rules && couple.length > 1000
 
           couple.length.times { polymer.delete_at(pointer) }
           new_element.reverse.each { polymer.insert(pointer, _1) }
+          add_to_rules(new_element) unless expanding_rules
 
           pointer += new_element.length - 1
         end
+        puts "expansion completed" unless expanding_rules
       end
 
       def find_largest_known_pattern(polymer, pointer)
-        end_pointer = pointer
-        until polymer[end_pointer + 1].nil?
-          pattern = polymer[(pointer..(end_pointer + 1))].join
-          return end_pointer unless rules.key?(pattern)
+        end_pointer = pointer + 1
+        result = pointer
+        largest_rule = rules.keys.map(&:size).max
 
+        until polymer[end_pointer].nil? || (end_pointer - pointer) >= largest_rule
+          pattern = polymer[pointer..end_pointer].join
+          result = end_pointer if rules[pattern]
           end_pointer += 1
         end
-        end_pointer
+        result
       end
 
       def submarine_polymer
@@ -81,17 +100,13 @@ module AdventOfCode2021
         end
       end
 
-      def expand_rules
-        new_rules = {}
-        rules.each do |_, value|
-          next unless rules[value].nil?
-          
-          expanded_value = value.dup
-          expand_polymer(expanded_value)
-          new_rules[value.join] = expanded_value
-        end
+      def add_to_rules(new_element)
+        return if rules.key?(new_element.join) || new_element.length >= 900
 
-        rules.merge! new_rules
+        @added_patterns += 1
+        expanded_value = new_element.dup
+        expand_polymer(expanded_value, true)
+        rules[new_element.join] = expanded_value
       end
     end
   end
