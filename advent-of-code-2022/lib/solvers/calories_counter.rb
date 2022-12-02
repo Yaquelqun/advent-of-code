653dbb05a02611cfcfcf442b7a2b35adb2e1a20b
Yaquelqun <sandjiv.parassou@gmail.com>
2022-12-02 12:07:37 +0100

    do part 1

--- advent-of-code-2022/lib/solvers/calories_counter.rb
+++ advent-of-code-2022/lib/solvers/calories_counter.rb
@@ -7,18 +7,36 @@ module AdventOfCode2022
     # finds low points in height map
     class CaloriesCounter
       def initialize
-        @data = Helpers::InputParser.new(endpoint: "day1_input").parse_data
-        @row_length = @data.first.length
+        @data = Helpers::InputParser.new(endpoint: "day1_input").parse_data.map(&:to_i)
+        @calory_max = 0
       end
 
       def solve
-        puts "highest calory count: #{20}" # Solution: 
+        puts "highest calory count: #{part1_solution}" # Solution: 68787
         puts " #{}" # Solution: 
       end
 
       private
 
       attr_reader :data
+
+      def part1_solution
+        current_max = 0
+        data.each do |count|
+          if count != 0
+            current_max += count
+            next
+          end
+
+          compare_calory_max(current_max)
+          current_max = 0
+        end
+        @calory_max
+      end
+
+      def compare_calory_max(potential_max)
+        @calory_max = potential_max if potential_max > @calory_max
+      end
     end
   end
 end
