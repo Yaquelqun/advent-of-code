9e7cb03ed5c483fe20389e5fbece01e1937e9e6f
Yaquelqun <sandjiv.parassou@gmail.com>
2022-12-03 16:14:22 +0100

    finish day 3

--- advent-of-code-2022/lib/solvers/rucksack_parser.rb
+++ advent-of-code-2022/lib/solvers/rucksack_parser.rb
@@ -10,7 +10,8 @@ module AdventOfCode2022
       end
 
       def solve
-        puts "common items priority sum: #{solve_part1}"
+        puts "individual common items priority sum: #{solve_part1}" # Solution 8515
+        puts "team common items priority sum: #{solve_part2}" # Solution 2434
       end
 
       private
@@ -19,22 +20,33 @@ module AdventOfCode2022
 
       def solve_part1
         @data.map { split_rucksack(_1) }
-             .map { find_common_element(*_1) }
+             .map { find_common_element(_1) }
              .map { convert_to_priority(_1) }
              .sum
+      end
+
+      def solve_part2
+        group_rucksacks_by_teams
+          .map { find_common_element(_1) }
+          .map { convert_to_priority(_1) }
+          .sum
       end
 
       def split_rucksack(rucksack_content)
         rucksack_content.each_slice(rucksack_content.length / 2).to_a
       end
 
-      def find_common_element(compartment1, compartment2)
-        (compartment1 & compartment2)[0]
+      def find_common_element(compartments)
+        compartments.reduce(compartments[0]) { _1 & _2 }[0]
       end
 
       PRIORITIES = ("a".."z").to_a + ("A".."Z").to_a
       def convert_to_priority(element)
         PRIORITIES.index(element) + 1
+      end
+
+      def group_rucksacks_by_teams
+        @data.each_slice(3).to_a
       end
     end
   end
