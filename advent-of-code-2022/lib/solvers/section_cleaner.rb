84bfddc9445c3b09ad2bb83ea6e96d82ebbbba0d
Yaquelqun <sandjiv.parassou@gmail.com>
2022-12-04 11:31:14 +0100

    refactor part 1 to be able to reuse the method in part2

--- advent-of-code-2022/lib/solvers/section_cleaner.rb
+++ advent-of-code-2022/lib/solvers/section_cleaner.rb
@@ -10,18 +10,19 @@ module AdventOfCode2022
       end
 
       def solve
-        puts "number of covered space: #{solve_part1}" # Solution: 453
+        puts "number of totally covered space: #{tally_covers(cover_option: "total")}" # Solution: 453
+        # puts "number of partially covered space: #{solve_part1}" # Solution: 
       end
 
       private
 
       attr_reader :data
 
-      def solve_part1
+      def tally_covers(cover_option:)
         @data.map { turn_to_ranges(_1) }
              .reduce(0) do |acc, ranges|
                range1, range2 = ranges
-               acc += 1 if range1.cover?(range2) || range2.cover?(range1)
+               acc += 1 if send("#{cover_option}_cover_between?", range1, range2)
                acc
              end
       end
@@ -29,6 +30,10 @@ module AdventOfCode2022
       def turn_to_ranges(inputs)
         inputs.map { _1.split("-").map(&:to_i) }
               .map { (_1[0].._1[1]) }
+      end
+
+      def total_cover_between?(range1, range2)
+        range1.cover?(range2) || range2.cover?(range1)
       end
     end
   end
