32bbf6a17b9cbfb4d9099a58a4aa15abda3e6fba
Yaquelqun <sandjiv.parassou@gmail.com>
2022-12-08 18:22:31 +0100

    finish part1

--- advent-of-code-2022/lib/solvers/tree_spotter.rb
+++ advent-of-code-2022/lib/solvers/tree_spotter.rb
@@ -22,13 +22,14 @@ module AdventOfCode2022
       DIRECTIONS = %w[up down left right].freeze
 
       def solve_part1
-        x, y = [0, 0]
-        count = 1
+        x = y = 0
+        count = 0
         while y <= max_y
+          count += 1 if DIRECTIONS.any? { send("visible_from_#{_1}?", x, y) }
           x, y = increment_counter(x: x, y: y)
         end
 
-        [x, y]
+        count
       end
 
       def increment_counter(x:, y:)
@@ -46,6 +47,34 @@ module AdventOfCode2022
 
       def max_y
         @max_y ||= @data.length - 1
+      end
+
+      def value_at(x, y)
+        @data[x][y]
+      end
+
+      def visible_from_up?(x, y)
+        return true if y.zero?
+
+        (0..(y - 1)).all? { @data[x][_1] < value_at(x, y)}
+      end
+
+      def visible_from_down?(x, y)
+        return true if y == max_y
+
+        ((y + 1)..max_y).all? { @data[x][_1] < value_at(x, y) }
+      end
+
+      def visible_from_left?(x, y)
+        return true if x.zero?
+
+        (0..(x - 1)).all? { @data[_1][y] < value_at(x, y)}
+      end
+
+      def visible_from_right?(x, y)
+        return true if x == max_x
+
+        ((x + 1)..max_x).all? { @data[_1][y] < value_at(x, y) }
       end
     end
   end
