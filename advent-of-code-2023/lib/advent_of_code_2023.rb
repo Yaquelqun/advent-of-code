1df625a2450ff7d41e6f30d6b672d3bd82c312cc
Yaquelqun <sandjiv.parassou@gmail.com>
2023-12-02 11:41:19 +0100

    still error

--- advent-of-code-2023/lib/advent_of_code_2023.rb
+++ advent-of-code-2023/lib/advent_of_code_2023.rb
@@ -6,11 +6,14 @@ require_relative "advent_of_code_2023/day_solver"
 module AdventOfCode2023
   class Error < StandardError; end
 
-  def self.run
+  def self.run(day)
     return puts "You checked out from the template, good luck for this year ^^" if name.include? "Pouet"
 
-    puts "Advent of Code 2023"
-    puts "what day do you want the solution to ?"
-    DaySolver.new(gets.chomp.to_i).solve
+    if day.nil?
+      puts "Advent of Code 2023"
+      puts "what day do you want the solution to ?"
+      day = gets.chomp
+    end
+    DaySolver.new(day.to_i).solve
   end
 end
--- advent-of-code-2023/run.rb
+++ advent-of-code-2023/run.rb
@@ -3,4 +3,4 @@
 require "byebug"
 require_relative "lib/advent_of_code_2023"
 
-AdventOfCode2023.run
+AdventOfCode2023.run(ARGV[0])
