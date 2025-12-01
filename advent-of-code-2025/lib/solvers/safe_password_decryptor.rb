f52040bdac1f8600f4a736dcbcad20c5b178e9a0
Yaquelqun <sandjiv.parassou@gmail.com>
2025-12-01 18:18:30 +0100

    added comments

--- advent-of-code-2025/lib/solvers/safe_password_decryptor.rb
+++ advent-of-code-2025/lib/solvers/safe_password_decryptor.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-# Day1 solver
+# Problem:
+# A safe rotary dial starts at 50, and we have a list of operations
+# that contain a direction and movement range (how much we turn the dial)
+# Given those information: 
+# - compute how many times the dial ended up on 0
+# - compute how many times the dial ended up or crossed 0 while turning
 module Solvers
   class SafePasswordDecryptor
     attr_reader :steps
@@ -8,8 +13,8 @@ module Solvers
     def initialize(input: "day1")
       @steps = Helpers::InputParser.new(input: input)
                                    .parse_data
-                                   .map { |step| [step[0], step[1..]] }
-                                   .map { |direction, amount| [direction == "L" ? "-" : "+", amount.to_i] }
+                                   .map { |step| [step[0], step[1..]] } # Split the input into [direction(L or R), amount]
+                                   .map { |direction, amount| [direction == "L" ? "-" : "+", amount.to_i] } # Replaces L and R by +/- to more easily compute stuff
     end
 
     def solve
@@ -44,11 +49,12 @@ module Solvers
       result
     end
 
+    # This method simulates the movement of the dial
     def move_dial(position, direction, amount)
-      complete_turns = amount / 100
-      remainder = amount.remainder(100)
-      final_position = position.send(direction, remainder) % 100
-      crossed_zero = crossed_zero?(direction, position, final_position)
+      complete_turns = amount / 100 # Complete turns are irrelevant until part 2
+      remainder = amount.remainder(100) # Remainder is the real movement
+      final_position = position.send(direction, remainder) % 100 # since we precomputed L/R into +/-, send is really cool now =D
+      crossed_zero = crossed_zero?(direction, position, final_position) # relevant for part 2
       {
         position:,
         direction:,
@@ -59,9 +65,15 @@ module Solvers
       }
     end
 
+    # This method checks if the idial crossed 0
     def crossed_zero?(direction, position, final_position)
-      return false if position.zero? || final_position.zero?
+      return false if position.zero? || final_position.zero? # if started or ended up on 0, we technically didn't cross it
 
+      # Looking at a dial,
+      # assuming that we haven't done more than a full turn (which we know since we removed them with complete_turns)
+      # we know that we crossed 0 if the dial end up before where it was going forward
+      # or after where it was going backward
+      # There's gotta be a better way of doing this, but it's getting late :p
       (direction == '+'  && final_position < position) || (direction == '-' && final_position > position)
     end
   end
