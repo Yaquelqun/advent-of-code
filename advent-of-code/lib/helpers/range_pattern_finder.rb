c25e2d929a8d6464e29746755b20db5d9c073952
Yaquelqun <sandjiv.parassou@gmail.com>
2025-12-02 15:12:34 +0100

    remove performance improvements to prepare for part 2

--- advent-of-code/lib/helpers/range_pattern_finder.rb
+++ advent-of-code/lib/helpers/range_pattern_finder.rb
@@ -9,15 +9,12 @@ module Helpers
     end
 
     def patterned_numbers
-      return [] if no_pattern_in_range?
-
-      # From that point we know that the first element of the range has an even number of numbers
       result = []
       pattern = first_pattern
       while 1==1
         number = (pattern.to_s*2).to_i
-        return result if number > reduced_range.last
-        if reduced_range.include?(number)
+        return result if number > range.last
+        if range.include?(number)
           puts "flagging #{number}"
           result << number.to_i 
         end
@@ -27,42 +24,11 @@ module Helpers
     end
 
     private
-
-    # If both range borders have the same amount of number
-    # and those numbers have an odd length, then we know there can't be patterns in there
-    def no_pattern_in_range?
-      first_element_length = range.first.to_s.length
-      first_element_length.odd? && range.last.to_s.length == first_element_length
-    end
-
-    # To check less things, we can remove odd-length numbers
-    # from the extremity of the range
-    # (there can still be some in the middle of the range, too lazy to remove em)
-    def reduced_range
-      @reduced_range ||= begin
-        first_element_length = range.first.to_s.length
-        new_first_element = if first_element_length.odd?
-                              10**first_element_length
-                            else
-                              range.first
-                            end
-        last_element_length = range.last.to_s.length
-        new_last_element = if last_element_length.odd?
-                            (10**(last_element_length - 1)) - 1
-                          else
-                            range.last
-                          end
-
-        result = (new_first_element..new_last_element)
-        puts "reduced_range from #{range.inspect} to #{result.inspect}"
-        result
-      end
-    end
  
     # I hate this...
     # Basically, take the first half numbers of the integer
     def first_pattern
-      string_first_element = reduced_range.first.to_s
+      string_first_element = range.first.to_s
       string_first_element[0..string_first_element.length/2 - 1].to_i
     end
   end
