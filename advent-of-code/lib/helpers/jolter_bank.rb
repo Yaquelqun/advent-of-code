122e5209e7cc660fa9d178bfa9b66ca198353d1a
Yaquelqun <sandjiv.parassou@gmail.com>
2025-12-03 12:48:19 +0100

    empty jolter bank model

---
+++ advent-of-code/lib/helpers/jolter_bank.rb
@@ -0,0 +1,25 @@
+module Helpers
+  class JolterBank
+    attr_reader :input_bank, :output_bank
+    def initialize(input_bank)
+      @input_bank = input_bank
+      @output_bank = []
+    end
+
+    def initialize_ouput
+      
+    end
+
+    def advance
+      
+    end
+
+    def empty?
+      
+    end
+
+    def integer_output
+      
+    end
+  end
+end
