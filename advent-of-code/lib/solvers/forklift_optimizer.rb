711caad14b3a9f38dfb1a650b4e641e76c219535
Yaquelqun <sandjiv.parassou@gmail.com>
2025-12-04 13:28:08 +0100

    part1

--- advent-of-code/lib/solvers/forklift_optimizer.rb
+++ advent-of-code/lib/solvers/forklift_optimizer.rb
@@ -5,7 +5,7 @@ module Solvers
   class ForkliftOptimizer
     attr_reader :grid
 
-    def initialize(input: "day3")
+    def initialize(input: "day4")
       # get each line and split them into single chars
       @grid = Helpers::InputParser.new(input: input)
                                     .parse_data
@@ -13,7 +13,7 @@ module Solvers
     end
 
     def solve
-      puts "parts1: #{solve_part1}" # 17412 is correct
+      puts "parts1: #{solve_part1}" # 1604 is correct
       # puts "parts2: #{solve_part2}" # 172681562473501 is correct
     end
 
